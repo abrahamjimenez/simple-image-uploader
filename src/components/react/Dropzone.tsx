7315f55b9dec4c0f171e3da1ef488e43150415f9
Abraham Jimenez <64721034+abrahamjimenez@users.noreply.github.com>
2025-06-29 22:04:36 -0600

    Code is setup before trying to upload to S3 bucket

--- src/components/react/Dropzone.tsx
+++ src/components/react/Dropzone.tsx
@@ -1,33 +1,41 @@
-import React, {useCallback} from 'react'
-import {useDropzone} from 'react-dropzone'
+import { useCallback } from 'react'
+import { useDropzone } from 'react-dropzone'
 import "./Dropzone.scss"
 
 const Dropzone = () => {
-    const onDrop = useCallback(async acceptedFiles => {
-        console.log("acceptedFiles", acceptedFiles)
+  const onDrop = useCallback(async acceptedFiles => {
+    const file = acceptedFiles[0]
 
-        const res = await fetch("/api/upload", { method: "POST"})
-        const data = await res.json()
+    console.log("dzfile", file);
 
-        console.log("data", data);
+    const formData = new FormData();
+    formData.append("file", file)
 
-    }, [])
-    const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
+    console.log(`Params: ${file}`)
 
-    return (
-        <div {...getRootProps()}>
-            <input {...getInputProps()} />
-            {
-                isDragActive ?
-                    <p>Drop the files here ...</p> :
-                    <div>
-                        <p className="headline">Drag and drop a file or <span
-                            className="accent-primary">browse files</span></p>
-                        <p className="accent-secondary">JPG, PNG, or GIF - Max file size 2MB</p>
-                    </div>
-            }
-        </div>
-    );
+    const res = await fetch("/api/upload", { method: "POST", body: formData })
+    const data = await res.json()
+
+    console.log("data", data);
+
+  }, [])
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop })
+
+
+  return (
+    <div {...getRootProps()}>
+      <input {...getInputProps()} />
+      {
+        isDragActive ?
+          <p>Drop the files here ...</p> :
+          <div>
+            <p className="headline">Drag and drop a file or <span
+              className="accent-primary">browse files</span></p>
+            <p className="accent-secondary">JPG, PNG, or GIF - Max file size 2MB</p>
+          </div>
+      }
+    </div>
+  );
 };
 
 export default Dropzone;
--- src/pages/api/upload.ts
+++ src/pages/api/upload.ts
@@ -1,35 +1,36 @@
-import type {APIRoute} from "astro";
+import type { APIRoute } from "astro";
 import { S3Client, ListBucketsCommand } from "@aws-sdk/client-s3";
 
 const client = new S3Client({
-    region: "us-east-2",
-    credentials: {
-        accessKeyId: import.meta.env.S3_ACCESS_KEY_ID,
-        secretAccessKey: import.meta.env.S3_SECRET_ACCESS_KEY
-    }
+  region: "us-east-2",
+  credentials: {
+    accessKeyId: import.meta.env.S3_ACCESS_KEY_ID,
+    secretAccessKey: import.meta.env.S3_SECRET_ACCESS_KEY
+  }
 });
 
 
 export const POST: APIRoute = async ({ request }) => {
-    try {
-        const command = new ListBucketsCommand();
-        const data = await client.send(command)
+  try {
+    const formData = await request.formData()
+    const file = formData.get("file")
+    console.log("file", file);
 
-        return new Response(
-            JSON.stringify({
-                message: "This was a POST!",
-                data: data.Buckets
-            }),
-        );
-    } catch (e) {
+    const command = new ListBucketsCommand();
+    const data = await client.send(command)
 
-        return new Response(
-            JSON.stringify({
-                error: "Failed to fetch s3 buckets"
-            }), {status: 500}
-        );
-    }
+    return new Response(
+      JSON.stringify({
+        message: "This was a POST!",
+        data: data.Buckets
+      }),
+    );
+  } catch (e) {
 
-
-
+    return new Response(
+      JSON.stringify({
+        error: "Failed to fetch s3 buckets"
+      }), { status: 500 }
+    );
+  }
 };
